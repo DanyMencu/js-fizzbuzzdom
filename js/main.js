6fc07dad5245f28b5dbf95a986442d270e39caf3
DanyMencu <dany69men@gmail.com>
2021-10-25 16:02:42 +0200

    Add division variables

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 //Ref
 const container = document.getElementById('container');
 
-//1. Generare gli square da 1 a 100
+//Generare gli square da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    container.innerHTML += `<div class="square square-${i}">${i}</div>`;
+    //Condizione se è un numero divisibile sia per 3 che per 5
+    if ( (i % 3 == 0) && (i % 5 == 0) ) {
+        container.innerHTML += `<div class="square fizzbuzz">FizzBuzz</div>`;
+    }
+    //Condizione se è un numero divisibile per 5
+    else if (i % 5 == 0) {
+        container.innerHTML += `<div class="square buzz">Buzz</div>`;
+    }
+    //Condizione se è un numero divisibile per 3
+    else if (i % 3 == 0) {
+        container.innerHTML += `<div class="square fizz">Fizz</div>`;
+    }
+    //Condizione di default
+    else {
+        container.innerHTML += `<div class="square">${i}</div>`;
+    }
 }
-//2. Creare la variabile dei nimeri divisibile per 3 o 5 o entrambi
