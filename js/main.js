a94aa2efb5badfd4dc0dc483909d2fef471d59e8
DanyMencu <dany69men@gmail.com>
2021-10-25 14:43:58 +0200

    Create the square table

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //Ref
-
+const container = document.getElementById('container');
 
 //1. Generare gli square da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    container.innerHTML += `<div class="square square-${i}">${i}</div>`;
+}
 //2. Creare la variabile dei nimeri divisibile per 3 o 5 o entrambi
